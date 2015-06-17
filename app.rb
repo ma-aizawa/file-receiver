9fb94baa396853b119ea0f30df0ee708af3f1d3a
Masahiro Aizawa <ma-aizawa@m3.com>
2015-06-17 11:24:07 +0900

    Add sleep and delete file after file added

--- app.rb
+++ app.rb
@@ -6,9 +6,10 @@ post '/photo' do
   return 400, 'NG' unless params[:file]
 
   path = "/var/tmp/files/#{params[:file][:filename]}"
-  File.delete(path) if File.exists?(path)
   File.open(path, 'wb') {|f| f.write params[:file][:tempfile].read }
 
+  sleep(10)
+  File.delete(path) if File.exists?(path)
   return 200, ''
 end
 
