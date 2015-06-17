ad8cdebdca7a9296fcb49706d907d6d3e4c4afb5
Masahiro Aizawa <ma-aizawa@m3.com>
2015-06-17 11:25:50 +0900

    Move delete file and sleep to before add file

--- app.rb
+++ app.rb
@@ -5,11 +5,12 @@ puts settings.environment
 post '/photo' do
   return 400, 'NG' unless params[:file]
 
-  path = "/var/tmp/files/#{params[:file][:filename]}"
+
+  File.delete(path) if File.exists?(path)
+  sleep(10)
+
   File.open(path, 'wb') {|f| f.write params[:file][:tempfile].read }
 
-  sleep(10)
-  File.delete(path) if File.exists?(path)
   return 200, ''
 end
 
