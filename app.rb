0a3076e8db70e5f2ed06d90eada27e367731ac2f
Masahiro Aizawa <ma-aizawa@m3.com>
2015-06-17 11:21:41 +0900

    Before add file, delete file.

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ post '/photo' do
   return 400, 'NG' unless params[:file]
 
   path = "/var/tmp/files/#{params[:file][:filename]}"
+  File.delete(path) if File.exists?(path)
   File.open(path, 'wb') {|f| f.write params[:file][:tempfile].read }
 
   return 200, ''
