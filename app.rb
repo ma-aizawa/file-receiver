e5749e35d8d0cb877ee36848ef07219fc9d3d476
Masahiro Aizawa <ma-aizawa@m3.com>
2015-06-17 11:31:29 +0900

    test

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ post '/photo' do
   return 400, 'NG' unless params[:file]
 
   path = "/var/tmp/files/#{params[:file][:filename]}"
-  File.delete(path) if File.exists?(path)
-  sleep(10)
 
   File.open(path, 'wb') {|f| f.write params[:file][:tempfile].read }
 
+  sleep(15)
+  File.delete(path) if File.exists?(path)
   return 200, ''
 end
 
